e327d5780bac792fc8ca8fea576ed59af0837f34
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2022-12-26 19:33:41 +0100

    В процессе
    На моменте фикса неработающего forEach

--- src/index.js
+++ src/index.js
@@ -4,61 +4,78 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
 // Ожидание окончания ввода и ключ
-const DEBOUNCE_DELAY = 300;
 const API_KEY = '32188250-5588add6a92db6c3bf4a2a30f';
 // Переменные
 const refs={
   input:document.querySelector('.input'),
   gallery:document.querySelector('.gallery'),
+  button:document.querySelector('.button'),
+  img:document.querySelector('.search'),
 };
 // Слушатель на ввод
-refs.input.addEventListener('input', debounce(search, DEBOUNCE_DELAY));
-// Из 7 работы галерея
-const galleryItemMarkup = creatGalleryMarkup(galleryItems);
-galleryItem.insertAdjacentHTML("beforeend", galleryItemMarkup);
+refs.button.addEventListener('click',  getContent);
 
-function creatGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<li>
-         <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
-       </li>`;
-    })
-    .join("");
-}
-const lightbox = new SimpleLightbox(".gallery__item", {
-  captionsData: "alt",
-  captionDelay: 250,
-});
-	
+
 // Пример с сайта библиотеки
-function getContent(){
-const URL = fetch("https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(search));
-console.log(refs.input);
-axios.get(URL).then(res=>res.data).then(({articles})=>render(articles)).catch(error=>console.log(error))
+function getContent(e,images){
+e.preventDefault();
+const URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(refs.input.value)+"&image_type=photo&orientation=horizontal&safesearch=true";
+fetch(URL).then(res=>{
+  if(res.status !== 200){
+    throw new Error(res.totalHits)
+  }
+
+  return res.json();
+}).then(({articles})=>{createImage(articles)});
+
+
+// axios.get(URL).then(res=>res.data).then(({articles})=>render(articles)).catch(error=>console.log(error))
 }
-refs.input.addEventListener('input',debounce(createImage, DEBOUNCE_DELAY));
-// Карточка
-function createImage(images) {
-  return images.map((image) => `<div class="photo-card">
-  <a href="${image.largeImageURL}">
-<img src="${image.webformatURL}" alt="${image.tags}" class="gallery-image" loading="lazy" />
+
+function createImage(articles) {
+  refs.gallery.innerHTML='';
+  articles.forEach(({downloads,likes,comments,views,tags,webformatURL,largeImageURL}) => {
+    const card = `<div class="photo-card">
+  <a href="${largeImageURL}">
+<img src="${webformatURL}" alt="${tags}" class="gallery-image" loading="lazy" />
 </a>
 <div class="info">
-  <p class="info-item">${image.likes}
+  <p class="info-item">${likes}
   <b>Likes</b>
   </p>
-  <p class="info-item">${image.views}
+  <p class="info-item">${views}
   <b>Views</b>
   </p>
-  <p class="info-item">${image.comments}
+  <p class="info-item">${comments}
   <b>Comments</b>
   </p>
-  <p class="info-item">${image.downloads}
+  <p class="info-item">${downloads}
   <b>Downloads</b>
   </p>
 </div>
-</div>`).join('');
+</div>`
+refs.gallery.insertAdjacentHTML('beforeend',card )});
 };
+
+
+
+// Из 7 работы галерея
+// const galleryItemMarkup = creatGalleryMarkup(galleryItems);
+// galleryItem.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+// function creatGalleryMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<li>
+//          <a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a>
+//        </li>`;
+//     })
+//     .join("");
+// }
+// const lightbox = new SimpleLightbox(".gallery__item", {
+//   captionsData: "alt",
+//   captionDelay: 250,
+// });
+	
