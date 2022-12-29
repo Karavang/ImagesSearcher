49690ee483fa2c0f994fe34c073120d7d7e74f79
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2022-12-30 00:55:39 +0100

    reworked and with normal workly notify

--- src/index.js
+++ src/index.js
@@ -1,61 +1,114 @@
-// 1.Сделать чтобы по скроллу прогружались картинки дальше(по дефолту 20 картинок,сделать 1 дефолт а дальше по скроллу)
-// 2.Раздел Пагинация в ДЗ
-// 3.Заменить fetch и then на Axios и async/await и обернуть это в try/catch
-// 4.Если успею,сделать Simplelightbox
-
-
-import throttle from 'lodash.throttle';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
-// import { galleryItems } from "./gallery-items.js";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
-// Ожидание окончания ввода и ключ
+
 const API_KEY = '32188250-5588add6a92db6c3bf4a2a30f';
-// Переменные
-const refs={
-  input:document.querySelector('.input'),
-  gallery:document.querySelector('.gallery'),
-  button:document.querySelector('.button'),
-  img:document.querySelector('.search'),
-};
-// Слушатель на ввод
-refs.button.addEventListener('click',  getContent);
 let currentPage = 1;
-let totalPages;
-let isEverythingLoaded;
-console.log(totalPages)
+let observerTarget = null;
+let scoller = 1;
+const refs = {
+  input: document.querySelector('.input'),
+  gallery: document.querySelector('.gallery'),
+  button: document.querySelector('.button'),
+  img: document.querySelector('.search'),
+};
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+const observer = new IntersectionObserver(entries => {
+  if (entries[0].intersectionRatio <= 0) return;
+  onScrollHandler();
+});
+
+refs.button.addEventListener('click',  getContent);
 // Получение данных
-function getContent(e,images){
+async function getContent(e){
 e.preventDefault();
-const URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(refs.input.value)+`&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
-fetch(URL).then(res=>{
-  if(res.status !== 200){
-    throw new Error(res.totalHits)
+scoller = 1;
+refs.gallery.innerHTML = '';
+const query = refs.input.value.trim();
+if (!query) return;
+
+const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
+try {
+  refs.gallery.innerHTML='';
+  const res = await axios.get(URL);
+  const { hits, total } = res.data
+  createImages(hits, total);
+  if(hits.length === total){ 
+    Notify.failure(`No more images to fetch.`, {
+      position: 'center-top',
+    });
+    return;
   }
-  return res.json();
-}).then(({hits,total})=>{createImage(hits,total)});
 
-
-// axios.get(URL).then(res=>res.data).then(({articles})=>render(articles)).catch(error=>console.log(error))
+  observerTarget = refs.gallery.lastElementChild;
+  if (observerTarget) observer.observe(observerTarget);
+} catch (error) {
+  console.log(error);
+}
 }
 
-// Функция на создание карточек
-const createImage = (hits,total) =>{
-  totalPages = total/40;
+async function onScrollHandler() {
+  observer.unobserve(observerTarget);
+  currentPage += 1;
+  const query = refs.input.value.trim();
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
 
-  refs.gallery.innerHTML='';
-  if(total!==0){ Notify.success(`Hooray! We found ${total} images.`, {
+  try {
+    const res = await axios.get(URL);
+    const { hits, totalHits } = res.data;
+    createImages(hits, totalHits);
+    scrollSmootly();
+    lightbox.refresh();
+
+    if(hits.length < 40){ 
+      Notify.failure(`No more images to fetch.`, {
+        position: 'center-top',
+      });
+      return;
+    }
+
+
+    observerTarget = refs.gallery.lastElementChild;
+    observer.observe(observerTarget);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function scrollSmootly() {
+  const { height: cardHeight } =
+    refs.gallery.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+
+function createImages (hits,total) {
+  console.log(total);
+  if(total!==0 && scoller===1){ 
+    Notify.success(`Hooray! We found ${total} images.`, {
     position: 'center-top',
-  });}
-  if(total===0){ Notify.failure(`Sorry, there are no images matching your search query. Please try again.`, {
+  })
+  scoller=2;}
+  if(total===0){ 
+    Notify.failure(`Sorry, there are no images matching your search query. Please try again.`, {
     position: 'center-top',
-  });}
-  hits.map(({downloads,likes,comments,views,tags,webformatURL,largeImageURL}) => {
-    const card = `<div class="photo-card">
-  <a href="${largeImageURL}">
+  });
+  return;
+}
+  const markup = hits.map(({downloads,likes,comments,views,tags,webformatURL,largeImageURL}) => {
+    return `<div class="photo-card">
+<a href="${largeImageURL}">
 <img src="${webformatURL}" alt="${tags}" class="gallery-image" loading="lazy" />
-</a>
 <div class="info">
   <p class="info-item">${likes}
   <b>Likes</b>
@@ -70,48 +123,12 @@ const createImage = (hits,total) =>{
   <b>Downloads</b>
   </p>
 </div>
+</a>
 </div>`
-refs.gallery.insertAdjacentHTML('beforeend',card )});
-
-
+}).join('');
+refs.gallery.insertAdjacentHTML('beforeend', markup);
+lightbox.refresh();
 };
-const lightbox = new SimpleLightbox(".gallery-image", {
-  captionsData: "alt",
-  captionDelay: 250,
-});
-
-// Подгрузка карточек(пока не работает,остановился на подключении функций и спросил у ментора)
-// window.addEventListener('load', async (e,images,hits,total) => {
-//   const articles = await getContent(e,images);
-//   const elements = createImage(hits,total);
-  
-//   refs.gallery.innerHTML = '';
-//   refs.gallery.insertAdjacentHTML('beforeend', elements)
-// })
-
-
-
-const throttled = throttle(async (e,images,articles)=>{
-  
-  if (isEverythingLoaded) return;
-
-  const body = document.body, html = document.documentElement;
-  const totalHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-  const pixelsToBottom = totalHeight - window.innerHeight - window.scrollY;
-  if (pixelsToBottom < 450) {
-    currentPage += 1;
-    isEverythingLoaded = currentPage >= totalPages
-    
-    const articles = await getContent(e,images);
-    const elements = createImage(articles);
-
-    refs.articles.insertAdjacentHTML('beforeend', elements)
-  }
-}, 500)
-
-window.addEventListener('scroll', throttled );
-
-
 
 
 
